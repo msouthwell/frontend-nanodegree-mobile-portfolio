33bf57f295f08ff060e75554d46acbe80cf9716c
msouth6 <matthewsouthwell13@gmail.com>
2016-04-11 20:17:20 +0900

    --

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,15 @@
-
 module.exports = function(grunt) {
-var mozjpeg = require('imagemin-mozjpeg');
+    var mozjpeg = require('imagemin-mozjpeg');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
             options: {
-              stripBanners: true,
-              banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-              '<%= grunt.template.today("yyyy-mm-dd") %> */',
-              },
-              js: {
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                    '<%= grunt.template.today("yyyy-mm-dd") %> */',
+            },
+            js: {
                 src: ['js/*.js'],
                 dest: 'dist/built.js'
             },
@@ -19,42 +18,45 @@ var mozjpeg = require('imagemin-mozjpeg');
                 dest: 'dist/styles.css'
             },
         },
-          uglify: {
+        uglify: {
             my_target: {
-              files: {
-                'js/perfmatters.min.js': 'js/perfmatters.js'
-              }
+                files: {
+                    'js/perfmatters.min.js': 'js/perfmatters.js'
+                }
             }
-          },
-          cssmin: {
-  options: {
-    shorthandCompacting: false,
-    roundingPrecision: -1
-  },
-  target: {
-    files: {
-      'css/print.min.css': ['css/print.css'],
-      'css/style.min.css': ['css/style.css']
-    }
-  }
-},
-  imagemin: {                          // Task
-    dynamic: {                          // Target
-      files: [{
-        expand: true,
-        cwd: 'img/',
-        src: ['**/*.{png,jpg,gif}'],
-        dest: 'img/'
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'css/print.min.css': ['css/print.css'],
+                    'css/style.min.css': ['css/style.css'],
+                    'views/css/bootstrap-grid.min.css': 'views/css/bootstrap-grid.css',
+                    'views/css/style.min.css' : 'views/css/style.css'
+                }
+            }
+        },
+        imagemin: { // Task
+            dynamic: { // Target
+                files: [{
+                    expand: true,
+                    cwd: 'img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'img/'
 
-      }]
-    }},
+                }]
+            }
+        },
         watch: {
             scripts: {
-            files: ['**/*.js'],
-            tasks: ['jshint'],
-            options: {
-                spawn: false,
-            },
+                files: ['**/*.js'],
+                tasks: ['jshint'],
+                options: {
+                    spawn: false,
+                },
             },
         },
     });
